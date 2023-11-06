158ad0a5221475c5a0a62abb0af755124a025add
HP <HP@HUNNY>
2023-11-06 14:31:39 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.WriteLine("This is my First Class of .NET Programming");
+            Console.ReadLine();
+        }
+    }
+}
